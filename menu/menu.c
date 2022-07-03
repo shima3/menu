619b4734632cc281a7f74eaed8acd1fe3b742e7d
root <shima@hiroshima-cu.ac.jp>
2022-07-03 21:08:35 +0900

    Successful compilation

--- menu/menu.c
+++ menu/menu.c
@@ -382,7 +382,7 @@ int main(int argc, char *argv[ ]){
     redrawChoice();
     refresh( );
     ch=getch( ); // キーボードから文字を入力する。
-    wprintf(commandWin, "(%d)", ch);
+    wprintw(commandWin, "(%d)", ch);
     // getyx(stdscr, y, x); // カーソルの座標を取得する。
     // wprintw(consoleWin, "ch=%d, x=%d, y=%d, w=%d, h=%d\n", ch, x, y, screenWidth, screenHeight); // curses版のprintf
     // printf("ch=%d, x=%d, y=%d, w=%d, h=%d\r\n", ch, x, y, screenWidth, screenHeight); // curses版のprintf
