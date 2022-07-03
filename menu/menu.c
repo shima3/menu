b1e4c7237aa45e235935e4f5410acaeb944e196f
root <shima@hiroshima-cu.ac.jp>
2022-07-03 19:38:04 +0900

    Successful compilation

--- menu/menu.c
+++ menu/menu.c
@@ -347,7 +347,7 @@ int main(int argc, char *argv[ ]){
   */
   
   for(;;){
-    write(fdm, "\001\013A\001\013", 2); fsync(fdm);
+    fputs("\001\013A\001\013", fdm); fsync(fdm);
     getmaxyx(stdscr, screenHeight, screenWidth); // スクリーンサイズを取得する。
     wresize(menuWin, screenHeight, menuWidth); // ウィンドウのサイズを変更する。
     consoleWidth=screenWidth-menuWidth;
