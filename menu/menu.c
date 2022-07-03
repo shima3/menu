abebdcac8cea2b6ee59bf3b7668716fd92517cab
root <shima@hiroshima-cu.ac.jp>
2022-07-03 13:41:03 +0900

    Successful compilation

--- menu/menu.c
+++ menu/menu.c
@@ -98,7 +98,7 @@ void redrawMenu(){
   // overlay(menuPad, menuWin);
   // overwrite(menuPad, menuWin);
   wmove(menuWin, 0, screenWidth-menuWidth);
-  for(i=0; i<screenHeight; ++i) waddstr(menuWin, "　\n");
+  for(i=0; i<screenHeight; ++i) waddstr(menuWin, "\000\n");
 
   int height=menuHeight-menuPadY;
   if(height>screenHeight) height=screenHeight;
