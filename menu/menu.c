4cdebec8b10aabb7503ec7aad59544b61fd977cf
root <shima@hiroshima-cu.ac.jp>
2022-07-03 19:49:18 +0900

    Successful compilation

--- menu/menu.c
+++ menu/menu.c
@@ -211,7 +211,8 @@ int main(int argc, char *argv[ ]){
   if(unlockpt(fdm)!=0) // 疑似端末の内部的なロックを解除する。
     perror("unlockpt");
   printf("main 1\n");
-  pid=fork( );
+  // pid=fork( );
+  pid=1;
   if(pid==0){ // child
     strcpy(buf, ptsname(fdm));
     close(fdm);
