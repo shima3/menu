b03bab182ca0141de40ac08110b985b18df172ee
root <shima@hiroshima-cu.ac.jp>
2022-07-09 07:07:20 +0900

    Sat Jul  9 07:07:19 JST 2022  M menu.c

--- menu.c
+++ menu.c
@@ -82,7 +82,8 @@
   #define CTRL(CH) ((CH)&0x1F)
 */
 #define STDERR_LOG fprintf(stderr, "%s %d: trace\n", __FILE__, __LINE__)
-#define debug_printf(fmt, ...) debug_fprintFLf(stderr, __FILE__, __LINE__, fmt, __VA_ARGS__)
+#define debug_printf(fmt, ...)
+// #define debug_printf(fmt, ...) debug_fprintFLf(stderr, __FILE__, __LINE__, fmt, __VA_ARGS__)
 
 void debug_fprintFLf(FILE *out, char file[ ], int line, char fmt[ ], ...){
   va_list ap;
